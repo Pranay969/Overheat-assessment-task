df9d0674239155b62ff4993c0f1f3251d9de102b
Pranay969 <pranay.chalapati@education.nsw.gov.au>
2024-11-11 10:37:39 +1100

    Delete homepage.js

--- homepage.js
+++
@@ -1,5 +0,0 @@
-// Function to load specific levels (example function for future use)
-function loadLevel(levelNumber) {
-    alert('Loading Level ' + levelNumber);
-    // Logic to load the selected level can go here
-}
