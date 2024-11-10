8c847eb4300c4d7fb5a60451dbda2a0252ad960d
Pranay969 <pranay.chalapati@education.nsw.gov.au>
2024-11-11 10:40:09 +1100

    Delete levels.js

--- levels.js
+++
@@ -1,9 +0,0 @@
-// Function to load a specific level
-function loadLevel(levelNumber) {
-  if (levelNumber === 1) {
-      // Redirect to the tutorial page for Level 1
-      window.location.href = 'tutorial.html'; // Ensure the file name matches the actual tutorial page name
-  } else {
-      alert('This level is locked! Complete the previous levels to unlock.');
-  }
-}
